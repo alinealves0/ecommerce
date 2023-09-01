71450ee9e9d7d0c9453949f2f6016675c7db73a0
Aline Alves <alinealves0@hotmail.com>
2023-09-01 18:00:35 -0300

    Alteração no views

--- perfil/views.py
+++ perfil/views.py
@@ -55,6 +55,10 @@ class Criar(BasePerfil):
     def post(self, *args, **kwargs):
         if not self.userform.is_valid() or not self.perfilform.is_valid():
             return self.renderizar
+        
+        username = self.userform.cleaned_data.get('username')
+        password = self.userform.cleaned_data.get('password')
+
         # Usuário logado
         if self.request.user.is_authenticated:
             pass
@@ -62,6 +66,8 @@ class Criar(BasePerfil):
         # Usuário não logado (novo)
         else:
             pass
+            # usuario = self.userform.save(commit=False)
+            # usuario.set_password()
 
         return self.renderizar
 
